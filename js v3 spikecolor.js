791bb47a050bf187682bd2385780bfa1b54efdc7
NetherWartQOL <98026411+NetherWartQOL@users.noreply.github.com>
2022-04-24 19:36:12 +0200

    Add files via upload

---
+++ js v3 spikecolor.js
@@ -0,0 +1,25 @@
+var password = prompt("Enter in your password!");
+        if (password=="E56G4Sgvegas6GVEa56AEGE87654ebEBGTJ665487u54k8646589žřčš7") {
+     setInterval(()=>{ // 444 = lava dragon // 230 = plane // 146 = emerald machine // 19 = reidite sword // 32 = reidite pickaxe // 34 = reidite spear // 39 = reidite hammer // 60 = lava helmet // 63 = lava sword // 82 = pilot //
+O[44][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967718946369314817/2.png"; // reidite_helmet
+    O[44][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967718946369314817/2.png";
+     O[19][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898470604820/3.png"; // reidite_sword
+         O[19][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898470604820/3.png";
+               O[146][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967736187093659669/1.png"; // emerald machine
+                      O[146][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967736187093659669/1.png";
+                         O[34][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898030178334/1.png"; //reidite spear
+                           O[34][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898030178334/1.png";
+                             O[39][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898877456424/1.png"; //reidite hammer
+                           O[39][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898877456424/1.png";
+},500)
+setInterval(function(){
+$("body").css('cursor', 'url(https://cdn.discordapp.com/attachments/965021405823529040/967711898680311858/spectral_set.png), default'); //just cursor
+$("#game_canvas").css('cursor', 'url(https://cdn.discordapp.com/attachments/965021405823529040/967711898680311858/spectral_set.png), default');
+$("button").css('cursor', 'url(https://cdn.discordapp.com/attachments/965021405823529040/967711898680311858/spectral_set.png), default');
+$("li").css('cursor', 'url(https://cdn.discordapp.com/attachments/965021405823529040/967711898680311858/spectral_set.png), default');
+    },10)
+        }
+        else
+        {
+            alert("Wrong password! Game started without textures!");
+        }
