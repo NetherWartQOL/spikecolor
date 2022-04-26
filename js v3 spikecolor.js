823e95356b40ba718fd3232fc88e6d20d7489ad0
CodingBlyat69 <98026411+NetherWartQOL@users.noreply.github.com>
2022-04-26 16:19:35 +0200

    Update js v3 spikecolor.js

--- js v3 spikecolor.js
+++ js v3 spikecolor.js
@@ -1,7 +1,7 @@
 var password = prompt("*Please enter your password!*");
 if (password== 'RSJUXtcgjTEk7HzdhWYVFBmKnPwpys68urfq52xbaNDGe4' | password== 'dUxuNGhSWkA9BTM4nJEKYf5te76qHDpXc3ywLVZzFQR2gC' | password =='qdXuEt5cJCRAxjKLMQrzHgbvB8hPG9V6my24Daf3STwWsF' | password =='gFS9LsX3CQnJWNYhKdackAexUv4t52GZR7rMjmyuq8BEb6' | password =='bemErPQ2a3SvzHuqKcGCWXZMjgRTNUwpFBJx58Dyn4Lf76' | password =='ykJMVnrCz6e42uRqxgfU39T57YtpdNQbhZFcwGHajsWKSP' | password =='qWNxHty4XkbnBVSv5aZ8z6AcmQJd9gp3rTKuUFRPLYswh2' | password =='nNREqpHhLCa54Uv8fbkerDc6wJtdTWAYg9jysSPX3GFKMz' | password =='D2CEFBWnRJKzNAbHdTMjkQpxeVsf8hqrtaP574gXY3UuZy' | password =='F4XCAfygczj2ndtxaHJZbrspLUWQKYeNq9EVw5Sk78uTPG') {
                 alert("All the textures were loaded successfully! :)");
-     setInterval(()=>{ // 444 = lava dragon // 230 = plane // 146 = emerald machine // 19 = reidite sword // 32 = reidite pickaxe // 34 = reidite spear // 39 = reidite hammer // 60 = lava helmet // 63 = lava sword // 82 = pilot //
+     setInterval(()=>{ // 444 = lava dragon // 230 = plane // 146 = emerald machine // 19 = reidite sword // 32 = reidite pickaxe // 34 = reidite spear // 39 = reidite hammer // 60 = lava helmet // 63 = lava sword // 82 = pilot // reidite sword = 19 // 81 = turban // 
 O[44][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967718946369314817/2.png"; // reidite_helmet
     O[44][1].src = "https://cdn.discordapp.com/attachments/965021405823529040/967718946369314817/2.png";
      O[19][0].src = "https://cdn.discordapp.com/attachments/965021405823529040/967711898470604820/3.png"; // reidite_sword
